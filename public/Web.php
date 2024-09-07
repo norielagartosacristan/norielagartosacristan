b05d725d99d463a2d606096b2cbeaf8d22c01e8d
norielagartosacristan <norielagartosacristan@gmail.com>
2024-09-07 12:28:58 +0800

    Update Web.php
    Added new text

---
+++ public/Web.php
@@ -0,0 +1,10 @@
+$url = isset($_GET['url']) ? $_GET['url'] : 'home';
+switch ($url) {
+    case 'cj-products':
+        $controller = new CJDropshippingController();
+        $controller->listProducts();
+        break;
+    default:
+        // Load default controller
+        break;
+}
