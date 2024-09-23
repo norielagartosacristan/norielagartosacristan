e762151f9ff083c2b48617a5ae552119af91053d
norielagartosacristan <norielagartosacristan@gmail.com>
2024-09-24 06:59:14 +0800

    Update index.php
    Update

--- public/index.php
+++ public/index.php
@@ -2,4 +2,6 @@
 require_once '../app/core/App.php';
 require_once '../app/core/Controller.php';
 
+require_once '../app/init.php'; // Initialize your app and autoload the classes
+
 $app = new App();
