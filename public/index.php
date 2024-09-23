cc33236e694c6c73163c531ee12a40538a936c48
norielagartosacristan <norielagartosacristan@gmail.com>
2024-09-24 07:01:24 +0800

    Add
    add new files

--- public/index.php
+++ public/index.php
@@ -1,7 +1,6 @@
 <?php
 require_once '../app/core/App.php';
 require_once '../app/core/Controller.php';
-
 require_once '../app/init.php'; // Initialize your app and autoload the classes
 
 $app = new App();
