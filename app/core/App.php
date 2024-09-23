3eb3690dd7c884f50e3d161c432aab250f5a10f4
norielagartosacristan <norielagartosacristan@gmail.com>
2024-09-24 07:26:44 +0800

    Add
    Add new

--- app/core/App.php
+++ app/core/App.php
@@ -1,6 +1,37 @@
 <?php
 
-class App {
+class App { 
+    protected $controller = 'ProductController';
+    protected $method = 'index';
+    protected $params =[];
+
+    public function _construct() {
+        $url = $this->parsURL();
+        if (file_exist('../appcontroller/' . ucfirst($url[0]) . 'Controller.php')) {
+            this->controller = ucfirst($url[0]) . 'Controller';
+            unset($url[0]);
+        }
+    }
+
+    require_once '../app/controllers/' . $this->controller . '.php';
+    $this->controller = new $this->controller;
+
+    if (isset($url[1])) {
+        if (method_exist($this->controller, $url[1])) {
+            $this->method = $url[1];
+            unset($url[1]);
+        }
+    }
+
+    $this->params =$url ? array_values($url) : [];
+    call_user_func_array([$this-controller, $this->method], $this->params);
+
+    public function parseUrl() {
+        if (isset($_GET['url'])) {
+            return explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+        }
+    }
+
     public function __construct() {
         $controller = isset($_GET['url']) ? $_GET['url'] : 'home';
         $this->loadController($controller);
